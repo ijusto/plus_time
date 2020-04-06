f2e9b2254be8c8eb5d2220c00827939247271555
Pedro Teixeira <pedro.teix@ua.pt>
2020-04-06 16:05:30 +0100

    Started Home Layout

--- plus_time/lib/home.dart
+++ plus_time/lib/home.dart
@@ -5,10 +5,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: HomePage(title: 'Flutter Demo Home Page'),
+      body: HomePage(title: 'Home'),
     );
   }
 }
@@ -33,8 +30,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _counter = 0;
+  int _selectedIndex = 0;
+  var projectsStats = [const _ProjectCard(
+          color: Colors.amber,
+          projectName: 'Mobile Computation',
+          projectType: '54h',
+    ),];
 
-  void _incrementCounter() {
+  void _addEvent() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
@@ -42,6 +45,12 @@ class _HomePageState extends State<HomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
     });
   }
 
@@ -54,51 +63,88 @@ class _HomePageState extends State<HomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: new AppBar(
+          title: Text(widget.title),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many timess:',
+              'What should I do next?',
             ),
+            Row(children: projectsStats),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              'Based on your location you should be here'
             ),
-            RaisedButton(
-          child: Text('Launch screen'),
-          onPressed: () {
-            // Navigate to the second screen using a named route.
-            Navigator.pushNamed(context, '/login');}),
+            Text('My projects'),
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          title: Text('Home'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.import_export),
+          title: Text('Import/export'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          title: Text('Settings'),
+        ),
+        BottomNavigationBarItem(
+        icon: Icon(Icons.exit_to_app),
+        title: Text('Logout'),
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.amber[800],
+      unselectedItemColor: Colors.black,
+      onTap: _onItemTapped,
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _addEvent,
+        tooltip: 'Add Event',
+        child: Icon(Icons.event),
+      ),
     );
   }
+}
+
+
+class _ProjectCard extends StatefulWidget {
+  _ProjectCard({Key key, this.color, this.name, this.time}) : super(key: key);
+
+  final Color  color;
+  final String name;
+  final String time;
+
+  @override
+  _ProjectCardState createState() => _ProjectCardState();
+
+}
+class _ProjectCardState extends  State<_ProjectCard> {
+  @override
+  Widget build(BuildContext context) {
+    	
+  return Container(
+    decoration: BoxDecoration(
+      color: widget.color,
+    ),
+    child: [
+
+    ]
+          title: Text(widget.name),
+          subtitle: Text(widget.time),
+        ),
+      ],
+    ),
+  );
+  }
+  
 }
--- plus_time/lib/main.dart
+++ plus_time/lib/main.dart
@@ -20,6 +20,7 @@ void main() {
       // Notice that the counter didn't reset back to zero; the application
       // is not restarted.
       primarySwatch: Colors.deepOrange,
+      
     ),
     initialRoute: '/',
     routes: {
