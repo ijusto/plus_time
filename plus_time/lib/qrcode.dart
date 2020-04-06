65e6d30786cbc848683220e76088e74be1d1ec42
Inês Justo <inesjusto@ua.pt>
2020-04-06 17:00:54 +0100

    qrcode widget

---
+++ plus_time/lib/qrcode.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
+
+class QRCode extends StatefulWidget {
+  @override
+  _QRCodeState createState() => _QRCodeState();
+}
+
+class _QRCodeState extends State<QRCode> {
+  String result = "Hey there";
+
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        result = qrResult;
+      });
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          result = "Camera permission was denied.";
+        });
+      } else {
+        setState(() {
+          result = "Someting went wrong: $e";
+        });
+      }
+    } on FormatException {
+      setState(() {
+        result = "You left before scanning anything.";
+      });
+    } catch (e) {
+      setState(() {
+        result = "Someting went wrong: $e";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Scanner"),
+      ),
+      body: Center(
+        child: Text(
+          result,
+          style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: _scanQR,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
