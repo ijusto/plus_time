8a40f8846b0326f723c43fbaa26fa548d24be8ed
Inês Justo <inesjusto@ua.pt>
2020-04-11 16:30:40 +0100

    reverted broken project

--- plus_time/android/app/src/main/kotlin/com/example/plus_time/MainActivity.kt
+++ plus_time/android/app/src/main/kotlin/com/example/plus_time/MainActivity.kt
@@ -5,39 +5,8 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.embedding.android.FlutterFragmentActivity
-/*
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.support.v4.content.FileProvider
 
-import java.io.File
-
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-*/
 class MainActivity: FlutterFragmentActivity() {
-    /*
-    private val SHARE_CHANNEL = "channel:me.alfian.share/share"
-
-    override fun onCreate(savedInstanceState: Bundle) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-
-        MethodChannel(flutterView, SHARE_CHANNEL).setMethodCallHandler { call, result ->
-            if (call.method.equals("shareFile")) {
-
-                val imageFile = File(this.getApplicationContext().getCacheDir(), call.arguments)
-                val contentUri = FileProvider.getUriForFile(this, "me.alfian.share", imageFile)
-                val shareIntent = Intent(Intent.ACTION_SEND)
-                shareIntent.setType("image/png")
-                shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri)
-
-                this.startActivity(Intent.createChooser(shareIntent, "Share image using"))
-            }
-        }
-    }
-    */
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
